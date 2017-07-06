2b45d167df2fbf15b0cfd8d71d201431c65e0ab8
sanpatnaik <sandeeppatnaik1991@gmail.com>
2017-07-06 16:24:55 +0530

    Update HelloControllerTest.java

--- src/test/java/rdemo/HelloControllerTest.java
+++ src/test/java/rdemo/HelloControllerTest.java
@@ -26,6 +26,6 @@ public class HelloControllerTest {
     public void getHello() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().string(contains("Greetings from Spring Boot!")));
+                .andExpect(content().string(containsString("Greetings")));
     }
 }
